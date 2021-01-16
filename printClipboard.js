977c5e281fc6b7f51bb96f93d06f0748875641cb
Voovs Selin <Voovs.13@gmail.com>
2021-01-16 15:02:07 -0700

    axios async fn

--- printClipboard.js
+++ printClipboard.js
@@ -1,4 +1,5 @@
 const clipboardy = require('clipboardy');
+const axios = require('axios');
 
 var oldclipboard = clipboardy.readSync();
 var newclipboard;
@@ -8,6 +9,17 @@ while (true) {
     if (oldclipboard != newclipboard) {
         console.log(newclipboard);
         oldclipboard = newclipboard
+
+		getPage();
     }
-	setTimeout(() => true, 500);
+	setTimeout(getPage, 500);
+}
+
+async function getPage() {
+	try {
+		const response = await axios.get('https://jisho.org/search/%E4%BD%95/');
+		console.log(response);
+	} catch (error) {
+		console.error(error);
+	}
 }
